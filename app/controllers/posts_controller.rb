a41da9bb22c5a13cd320639bb04401d59e7d0c58
vgty <victor.goutay@gmail.com>
2017-02-07 20:12:03 +0200

    Adding configuring devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user! , except: [:index, :show]
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -13,6 +14,7 @@ class PostsController < ApplicationController
   
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
     if @post.save
       redirect_to @post
     else
